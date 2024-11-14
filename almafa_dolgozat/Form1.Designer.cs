789ba5111ff0fd3b307290ebee1d5c7aed4a9f35
palanki.matyas <palanki.matyas@C19-06>
2024-11-14 18:19:23 +0100

    az ember mozog, az alma esik, de még nem kapja el az almát

--- almafa_dolgozat/Form1.Designer.cs
+++ almafa_dolgozat/Form1.Designer.cs
@@ -29,13 +29,119 @@ namespace almafa_dolgozat
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Head = new System.Windows.Forms.PictureBox();
+            this.Body = new System.Windows.Forms.PictureBox();
+            this.Hand = new System.Windows.Forms.PictureBox();
+            this.Kosar = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.Torzs = new System.Windows.Forms.PictureBox();
+            this.Lomb = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Head)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Body)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hand)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kosar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Torzs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lomb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Head
+            // 
+            this.Head.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.Head.Location = new System.Drawing.Point(427, 169);
+            this.Head.Name = "Head";
+            this.Head.Size = new System.Drawing.Size(44, 41);
+            this.Head.TabIndex = 0;
+            this.Head.TabStop = false;
+            // 
+            // Body
+            // 
+            this.Body.BackColor = System.Drawing.Color.Blue;
+            this.Body.Location = new System.Drawing.Point(441, 216);
+            this.Body.Name = "Body";
+            this.Body.Size = new System.Drawing.Size(16, 115);
+            this.Body.TabIndex = 1;
+            this.Body.TabStop = false;
+            // 
+            // Hand
+            // 
+            this.Hand.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.Hand.Location = new System.Drawing.Point(395, 241);
+            this.Hand.Name = "Hand";
+            this.Hand.Size = new System.Drawing.Size(40, 18);
+            this.Hand.TabIndex = 2;
+            this.Hand.TabStop = false;
+            // 
+            // Kosar
+            // 
+            this.Kosar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Kosar.Location = new System.Drawing.Point(597, 277);
+            this.Kosar.Name = "Kosar";
+            this.Kosar.Size = new System.Drawing.Size(110, 54);
+            this.Kosar.TabIndex = 3;
+            this.Kosar.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.pictureBox5.Location = new System.Drawing.Point(644, 241);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(15, 46);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Torzs
+            // 
+            this.Torzs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.Torzs.Location = new System.Drawing.Point(122, 134);
+            this.Torzs.Name = "Torzs";
+            this.Torzs.Size = new System.Drawing.Size(46, 197);
+            this.Torzs.TabIndex = 5;
+            this.Torzs.TabStop = false;
+            // 
+            // Lomb
+            // 
+            this.Lomb.BackColor = System.Drawing.Color.Green;
+            this.Lomb.Location = new System.Drawing.Point(54, 12);
+            this.Lomb.Name = "Lomb";
+            this.Lomb.Size = new System.Drawing.Size(177, 123);
+            this.Lomb.TabIndex = 6;
+            this.Lomb.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Lomb);
+            this.Controls.Add(this.Torzs);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.Kosar);
+            this.Controls.Add(this.Hand);
+            this.Controls.Add(this.Body);
+            this.Controls.Add(this.Head);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Head)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Body)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Hand)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Kosar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Torzs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lomb)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Head;
+        private System.Windows.Forms.PictureBox Body;
+        private System.Windows.Forms.PictureBox Hand;
+        private System.Windows.Forms.PictureBox Kosar;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox Torzs;
+        private System.Windows.Forms.PictureBox Lomb;
     }
 }
 
--- almafa_dolgozat/Form1.cs
+++ almafa_dolgozat/Form1.cs
@@ -12,14 +12,107 @@ namespace almafa_dolgozat
 {
     public partial class Form1 : Form
     {
+        Timer BodyMoveTimer = new Timer();
+        Timer HandMoveTimer = new Timer();
+        Timer AppleFallTimer = new Timer();
+        PictureBox alma = new PictureBox();
+        bool almaFall = false;
+        bool UtoKez = false;
+        bool LeftMove = true;
+        int num = 0;
         public Form1()
         {
             InitializeComponent();
             Start();
+            AlmaEsikEvent();
         }
         void Start()
         {
+            Label pontszam = new Label();
+            this.Controls.Add(pontszam);
+            alma.Top = (Lomb.Height + 5);
+            alma.Left = (Lomb.Width + 5);
+            pontszam.Top = 30;
+            pontszam.Left = 400;
+            pontszam.BackColor = Color.Gray;
 
+            
+            this.Controls.Add(alma);
+            alma.BackColor = Color.Red;
+            alma.Height = 15;
+            alma.Width = 15;
+
+            BodyMoveTimer.Interval = 15;
+            BodyMoveTimer.Tick += (s, e) =>
+            {
+                if(LeftMove && Hand.Left >= Torzs.Right)
+                {
+                    if(Hand.Left > Torzs.Right && !UtoKez)
+                    {
+                        Hand.Left -= 1;
+                        Head.Left -= 1;
+                        Body.Left -= 1;
+                    }
+                    else if(Hand.Left == Torzs.Right && UtoKez)
+                    {
+                        
+                        UtoKez = true;
+                        HandMoveTimer.Start();
+                        almaFall = true; 
+                    }
+                }
+                else if (!LeftMove)
+                {
+                    if(Kosar.Left != Hand.Left - 5)
+                    {
+                        Hand.Left += 1;
+                        Head.Left += 1;
+                        Body.Left += 1;
+                        alma.Left += 1;
+                    }
+                    else
+                    {
+                        if(alma.Top != Kosar.Top)
+                            alma.Top += 1;
+
+                        if (alma.Top == Kosar.Top)
+                            num++;
+                    }
+                }
+            };
+            BodyMoveTimer.Start();
+
+            HandMoveTimer.Interval = 10;
+            HandMoveTimer.Tick += (ss, ee) =>
+            {
+                if (Hand.Left == Torzs.Right)
+                {
+                    Hand.Left -= 15;
+                }
+                else
+                {
+                    Hand.Left += 15;
+                }
+            };
+
+
+        }
+        void AlmaEsikEvent()
+        {
+            AppleFallTimer.Interval = 10;
+            AppleFallTimer.Start();
+            AppleFallTimer.Tick += (s, e) =>
+            {
+                if (Hand.Left == Torzs.Right)
+                {
+                    alma.Top += 1;
+                }
+                //else if(Hand.Top == alma.Bottom)
+                //{
+                    
+                //}
+            };
+            
         }
     }
 }
